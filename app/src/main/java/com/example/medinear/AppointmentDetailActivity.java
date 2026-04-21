08789fe4e79299e4511b88a334ab5b61d8037a75
shahd-work <hamammichahd@gmail.com>
2026-04-21 23:11:21 +0100

    haw lkhedma

--- app/src/main/java/com/example/medinear/AppointmentDetailActivity.java
+++ app/src/main/java/com/example/medinear/AppointmentDetailActivity.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
 public class AppointmentDetailActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
